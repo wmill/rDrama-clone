90b1a2f506aa967c39aca5febd848299ce8f8d11
Walter Miller <walter@waltermiller.ca>
2026-03-17 04:33:06 -0400

    fix ts issue

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,20 @@
-import { config } from 'dotenv'
-import { defineConfig } from 'drizzle-kit'
+import { config } from "dotenv";
+import { defineConfig } from "drizzle-kit";
 
-config({ path: '.env.local' })
-config({ path: '.env' })
+config({ path: ".env.local" });
+config({ path: ".env" });
+
+const databaseUrl = process.env.DATABASE_URL;
+
+if (!databaseUrl) {
+	throw new Error("DATABASE_URL is not set");
+}
 
 export default defineConfig({
-  out: './drizzle',
-  schema: './src/db/schema.ts',
-  dialect: 'postgresql',
-  dbCredentials: {
-    url: process.env.DATABASE_URL,
-  },
-})
+	out: "./drizzle",
+	schema: "./src/db/schema.ts",
+	dialect: "postgresql",
+	dbCredentials: {
+		url: databaseUrl,
+	},
+});
